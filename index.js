cb4d657362f7ef49fd12b310f2bd650417b76225
Umme-Habiba123 <habibaislammim@gmail.com>
2026-02-15 23:24:54 +0600

    implement the patch methode

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server	(optional starting in v4.7 )
     // await client.connect();
 
     const db = client.db("ingridish-db");
-    const ingridishCollections = db.collection("ingridish");
+    const ingridishCollections = db.collection("ingridish") ;
 
     // Get all products
     app.get("/api/products", async (req, res) => {
